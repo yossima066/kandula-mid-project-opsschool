229e4a91a3cf8ed938ae0445aa2672c0e7c16df0
yossima066 <yossima066@gmail.com>
2022-03-29 00:21:18 +0300

    delete url

--- modules/monitoring/grafana/grafan.py
+++ modules/monitoring/grafana/grafan.py
@@ -13,7 +13,7 @@ for filename in os.listdir(directory):
         headers = CaseInsensitiveDict()
         headers["Accept"] = "application/json"
         headers["Content-Type"] = "application/json"
-        headers["Authorization"] = "Bearer eyJrIjoiU3ZicGEySk5NTWRLazlKbXBGZFhXSW9jTFVDSGt3Uk8iLCJuIjoibmFtZSIsImlkIjoxfQ== "
+        headers["Authorization"] = "Bearer  "
         url = 'http://web-lb-1854982176.us-east-1.elb.amazonaws.com:3000/api/dashboards/db'
         x = requests.post(url, json = data, headers = headers)
 
